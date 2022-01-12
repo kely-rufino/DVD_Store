e4f019e9b3ecdc30c587a271a362c012b7fa37bf
Kely Rufino <87963324+kely-rufino@users.noreply.github.com>
2022-01-12 21:31:21 +0000

    task 5: Adding filter by genre

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,25 +1,34 @@
-import { useState } from 'react';
-import MeetupList from '../components/meetups/MeetupList';
-import classes from './AllMeetup.css';
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
+import FilterGenre from "../components/meetups/FilterGenre";
+import MeetupList from "../components/meetups/MeetupList";
+import classes from "./AllMeetup.css";
 
-      
 function AllMeetupsPage() {
   const [loadedMeetups, setLoaderMeetups] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(" ");
 
+  useEffect(() => {
+    fetch("DVDdata.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setLoaderMeetups(data);
+      });
+  }, []);
 
-        fetch ("DVDdata.json")
-        .then(response => {
-            return response.json();
-        }).then(data =>{
-            setLoaderMeetups(data);
-        });
+  const filteredMovies = loadedMeetups.filter((item) => {
+    return selectedGenre === " " ? true : item.genre.includes(selectedGenre);
+  });
 
-        return (
-          <section className={classes.display}>
-            <h1>All Meetups</h1>
-            <MeetupList meetups={loadedMeetups}/>
-          </section>
-        );
-      }
-      
-      export default AllMeetupsPage;
+  return (
+    <section className={classes.display}>
+      <h1 className="h1"> All Meetups</h1>
+      <FilterGenre onChange={setSelectedGenre} selectedGenre={selectedGenre} />
+      <MeetupList meetups={filteredMovies} />
+    </section>
+  );
+}
+
+export default AllMeetupsPage;
