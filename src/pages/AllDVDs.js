53c263756db8f12afa256e14a5fbd700e3973b73
Kely Rufino <87963324+kely-rufino@users.noreply.github.com>
2022-01-14 14:07:09 +0000

    add filter by genre

--- src/pages/AllDVDs.js
+++ src/pages/AllDVDs.js
@@ -1,22 +1,30 @@
 import { useState } from 'react';
+import FilterGenre from '../components/DVDsItemsandlists/FilterGenre';
 import DVDsList from '../components/DVDsItemsandlists/DVDsList';
 
       
 function AllDVDsPage() {
-  const [DVDsStorge, LoadDVDsStorge] = useState([]);
+  const [DVDsStorage, LoadDVDsStorage] = useState([]);
+  const [selectedGenre, setSelectedGenre] = useState(" ");
+  
+  const filteredMovies = DVDsStorage.filter((item) => {
+    return selectedGenre === " " ? true : item.genre.includes(selectedGenre);
+  });
 
 
         fetch("DVDdata.json")
         .then(response => {
             return response.json();
         }).then(data =>{
-            LoadDVDsStorge(data);
+            LoadDVDsStorage(data);
         });
+        
 
         return (
           <section>
             <h1>All DVDs</h1>
-            <DVDsList DVDs={DVDsStorge}/>
+            <FilterGenre onChange={setSelectedGenre} />
+            <DVDsList DVDs={filteredMovies} selectedGenre={selectedGenre}/>
           </section>
         );
       }
