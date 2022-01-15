6a6f8737e33f1b1f7bffad8ea7562f6a3d21602d
Kely Rufino <87963324+kely-rufino@users.noreply.github.com>
2022-01-15 12:21:26 +0000

    adding useEffect for fetch and fix dvd filter

--- src/pages/AllDVDs.js
+++ src/pages/AllDVDs.js
@@ -1,32 +1,38 @@
-import { useState } from 'react';
-import FilterGenre from '../components/DVDsItemsandlists/FilterGenre';
-import DVDsList from '../components/DVDsItemsandlists/DVDsList';
+import { useEffect, useState } from "react";
+import FilterGenre from "../components/DVDsItemsandlists/FilterGenre";
+import DVDsList from "../components/DVDsItemsandlists/DVDsList";
+import { Pagination } from "@material-ui/lab";
 
-      
 function AllDVDsPage() {
-  const [DVDsStorage, LoadDVDsStorage] = useState([]);
+  const [DVDsStorage, loadDVDsStorage] = useState([]);
+
   const [selectedGenre, setSelectedGenre] = useState(" ");
-  
+
   const filteredMovies = DVDsStorage.filter((item) => {
     return selectedGenre === " " ? true : item.genre.includes(selectedGenre);
   });
 
+  useEffect(() => {
+    fetch("DVDdata.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        loadDVDsStorage(data);
+      });
+  }, []);
 
-        fetch("DVDdata.json")
-        .then(response => {
-            return response.json();
-        }).then(data =>{
-            LoadDVDsStorage(data);
-        });
-        
+  console.log({});
+  return (
+    <section>
+      <div>
+        <Pagination count={10} color="primary" />
+      </div>
+      <h1>All DVDs</h1>
+      <FilterGenre onChange={setSelectedGenre} selectedGenre={selectedGenre} />
+      <DVDsList DVDs={filteredMovies} />
+    </section>
+  );
+}
 
-        return (
-          <section>
-            <h1>All DVDs</h1>
-            <FilterGenre onChange={setSelectedGenre} />
-            <DVDsList DVDs={filteredMovies} selectedGenre={selectedGenre}/>
-          </section>
-        );
-      }
-      
-      export default AllDVDsPage;
+export default AllDVDsPage;
